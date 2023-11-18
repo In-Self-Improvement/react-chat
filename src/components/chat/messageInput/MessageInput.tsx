75f1ee49574556c1fac955947060873d2841685e
Bong <borobong230@gmail.com>
2023-11-19 02:27:38 +0900

    feat: firebase DB를 이용한 채팅 앱 구성

--- src/components/chat/messageInput/MessageInput.tsx
+++ src/components/chat/messageInput/MessageInput.tsx
@@ -1,25 +1,48 @@
+import React, { useState } from "react";
+import { auth, db } from "@/firebase";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { set } from "firebase/database";
+
 const MessageInput = () => {
   // 메시지 전송 로직을 여기에 추가합니다.
-  const sendMessage = () => {
-    // 메시지 보내는 로직
-  };
+  const [message, setMessage] = useState("");
 
+  const sendMessage = async (event: any) => {
+    event.preventDefault();
+    console.log("message", message);
+
+    if (message.trim() === "") {
+      alert("Enter valid message");
+      return;
+    }
+    const uid = auth?.currentUser?.uid || "";
+    const displayName = auth?.currentUser?.displayName || "이름없음";
+
+    await addDoc(collection(db, "messages"), {
+      text: message,
+      name: displayName,
+      createdAt: Date.now(),
+      uid,
+      photoURL: auth?.currentUser?.photoURL,
+    });
+    setMessage("");
+  };
   return (
-    <div className="flex items-center p-4">
+    <form className="flex items-center p-4" onSubmit={sendMessage}>
       <input
         type="text"
         placeholder="메시지를 입력하세요."
         className="flex-grow p-2 border border-gray-300 rounded-lg m-2 text-black focus:outline-none focus:ring-2 focus:ring-blue-500"
-        onKeyDown={(e) => e.key === "Enter" && sendMessage()}
+        onChange={(e) => setMessage(e.target.value)}
+        value={message}
       />
       <button
-        onClick={sendMessage}
         className="bg-blue-500 hover:bg-blue-600 text-black px-4 rounded-lg"
-        type="button"
+        type="submit"
       >
         전송
       </button>
-    </div>
+    </form>
   );
 };
 
